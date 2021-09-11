19d7bf0996cbcd7bbe5657783a70ae5052734db1
Joshua James Dela Cruz <delapzz1@gmail.com>
2021-09-11 22:45:12 +0800

    this is my react app

--- scripts/markdown.js
+++ scripts/markdown.js
@@ -1,5 +1,5 @@
 function App() {
-    return <div>Hello World</div>;
+    return <div>Hello World this is my first react webpage</div>;
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
