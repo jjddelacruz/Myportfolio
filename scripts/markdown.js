893b2ad6f1f5d335bc717e50309ba4f1d47f29ac
Joshua James Dela Cruz <delapzz1@gmail.com>
2021-09-13 12:36:17 +0800

    updating markdown previewer

--- scripts/markdown.js
+++ scripts/markdown.js
@@ -1,5 +1,40 @@
-function App() {
-    return <div>Hello World this is my first react webpage</div>;
-}
+marked.setOptions({
+    breaks: true
+})
 
+const renderer = new marked.Renderer();
+
+function App() {
+
+    const [text, setText] = React.useState("");
+
+    return (
+    <div className='text-center px-4'> 
+                  <h1 className='p-4'>My mark down previewer</h1>
+                  <textarea
+                  name='text'
+                  id='editor'
+                  rows='10'
+                  value={text}
+                  onChange={ (e) => setText(e.target.value) }
+                  className='textarea'
+                  ></textarea>
+                  <h3 className='mt-3'>Output</h3>
+
+                  <Preview markdown={text} />
+    </div>
+          );
+
+    function Preview({markdown}){
+        return (
+            <div
+             dangerouslySetInnerHTML={{
+                 __html: marked(markdown, {rendere: renderer} ),
+             }}
+             id='preview'
+            >
+            </div>
+        )
+    }
+}
 ReactDOM.render(<App />, document.getElementById('root'));
